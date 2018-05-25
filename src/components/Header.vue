232aa3d2c9ae454f845bfc1c425d7ac5fb6086ee
michaelmiller2116 <michael.miller2116@gmail.com>
2018-05-25 11:00:15 -0600

    added h1 and font family to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-
+    <h1>LiveWyre</h1>
   </header>
 </template>
 
@@ -11,10 +11,14 @@ export default {
 </script>
 
 <style scoped>
+
+@import url("https://fonts.googleapis.com/css?family=Permanent+Marker");
+
 header {
   background-color: #b760e6;
   padding: 1.2%;
   padding-bottom: 3.9%;
+  font-family: 'Permanent Marker', cursive;
 }
 
 </style>
