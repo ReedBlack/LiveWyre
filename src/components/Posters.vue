8d96327913769428915323b984229e7fdb72599a
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-05-25 11:38:46 -0600

    committing

--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -1,13 +1,30 @@
 <template>
-    <ul class="poster-grid">
-        <Poster class="poster" />
-    </ul>
+    <section>
+        <h2>your shows</h2>
+        <ul class="poster-grid">
+            <Poster class="poster" :posters="posters" />
+        </ul>
+    </section>
 </template>
 
 <script>
-export default {};
+import Poster from "@/components/Poster";
+
+export default {
+  name: "Posters",
+  components: {
+    Poster
+  }
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Permanent+Marker");
+
+h2 {
+  color: #b760e6;
+
+  font-family: "Permanent Marker", cursive;
+}
 </style>
 
