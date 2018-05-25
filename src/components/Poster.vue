7325baed8e889aedba04b3e50a6512a13728e633
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-05-25 14:29:34 -0600

    poster add

--- src/components/Poster.vue
+++ src/components/Poster.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="poster">
+    <div class="poster-box">
         <li v-for="show in shows" :show="show" :key="show.artist">
             <h3>{{show.artist}}</h3>
             <small>{{show.date}}</small>
@@ -17,10 +17,20 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.poster-box {
+  margin: 5px;
+  height: 100px;
+  width: 100px;
+  background-color: hsl(166, 100, 45);
+}
 img {
   max-height: 300px;
   max-width: 300px;
   background-size: 12%;
+}
+
+li {
+  list-style-type: none;
 }
 </style>
